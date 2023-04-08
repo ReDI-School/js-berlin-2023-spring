315043651d6e0e14447f9b6bed466618388cc4a7
Sevtap Fernengel <sevtap@plato.fritz.box>
2023-04-08 17:35:23 +0200

    Add lesson 8

---
+++ 2023-04-11-intro-objects/exercise3.js
@@ -0,0 +1,10 @@
+console.log("Hi, my name is " + me.name + ".");
+console.log("I am " + me.age + " years-old.")
+console.log("My favourite food is " + me.favouriteFood + ".");
+console.log("I enjoy listening to " + me.favouriteMusic + ".");
+
+if (me.isMarried) {
+    console.log("I am married");
+} else {
+    console.log("I am not married");
+}
